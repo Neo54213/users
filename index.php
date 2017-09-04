d91c8e3ceca9cb131d94b76dca273b00fe09b435
Stas <neo54213@mail.ru>
2017-09-04 13:07:17 +0300

    Get data from database

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 require_once 'db_connection.php';
 
-$link = mysqli_connect($host, $user, $password, $database) or die("������ " . mysqli_error($link));
+$link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
+mysqli_set_charset($link, 'utf8');
+
+$query ="SELECT * FROM users";
+$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+if($result){
+    $row = mysqli_fetch_array($result, MYSQLI_NUM);
+    var_dump($row);
+}
 
 mysqli_close($link);
