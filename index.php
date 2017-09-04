e03eb9bdfd70cf4eea71f368e29da284c176b43e
Stas <neo54213@mail.ru>
2017-09-04 19:11:57 +0300

    Added menu

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+$id = $_GET['id'];
+?>
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+<p>
+<?php
 require_once 'db_connection.php';
 
 $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
@@ -8,16 +18,26 @@ $query ="SELECT * FROM users";
 $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
 
 if($result){
-    while(true){
-        $row = mysqli_fetch_array($result, MYSQLI_NUM);
-
-        if(!$row){
-            return;
-        }
-
+    while($row = mysqli_fetch_array($result, MYSQLI_NUM)){
         var_dump($row);
         echo "<br>";
     }
 }
 
 mysqli_close($link);
+?>
+</p>
+<ul>
+    <li><a href="index.php?id=sign_in">Вход</a></li>
+    <li><a href="index.php?id=add">Добавить пользователя</a></li>
+    <li><a href="index.php?id=users">Отобразить всех пользователей</a></li>
+    <li><a href="index.php?id=">Редактировать профиль</a></li>
+</ul>
+<p>
+    <?php
+        require_once 'inc/routing.php';
+    ?>
+</p>
+
+</body>
+</html>
